d14d50688141a48ce6302c9a701bdcff5a52bf1f
hyeongwoo.lim <lkslyj2@naver.com>
2021-11-27 08:59:52 +0900

    feat: ScoreData class is a DTO that carries current score data

---
+++ src/baseball/ScoreData.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class ScoreData {
+    private int strike;
+    private int ball;
+
+    public ScoreData (int strike, int ball) {
+        this.ball = ball;
+        this.strike = strike;
+    }
+}
