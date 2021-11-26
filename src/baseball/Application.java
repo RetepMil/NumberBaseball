99c08dbbd6e12c6ac6c099d4c2f10612cb9d6e99
hyeongwoo.lim <lkslyj2@naver.com>
2021-11-27 08:54:58 +0900

    refactor: game control in Application.java is deleted. Similar process is now in Game.java

--- src/baseball/Application.java
+++ src/baseball/Application.java
@@ -2,12 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        do {
-            Game game = new Game();
-            game.startGame();
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        } while (UserInputController.askIntergetToBoolean(1, 2));
-        System.out.println("게임이 완전히 종료되었습니다.");
+        Game game = new Game();
+        game.startGame();
     }
 }
--- src/baseball/Game.java
+++ src/baseball/Game.java
@@ -1,22 +1,30 @@
 package baseball;
 
-
-import utils.RandomUtils;
-
 import java.util.ArrayList;
 
 public class Game {
     GameState gameState;
+    UserInputController userInput;
 
     public Game() {
         this.gameState = new GameState();
+        this.userInput = new UserInputController();
     }
 
     public void startGame() {
-        //Repeat asking process
+        do {
+            //Repeat asking process
+            gameProcess();
+        } while (UserInputController.askIntergetToBoolean(1, 2));
+        System.out.println("게임이 완전히 종료되었습니다.");
+    }
+
+    private void gameProcess() {
         do {
             askForGuesses();
         } while (!gameState.isWin());
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
     private void askForGuesses() {
