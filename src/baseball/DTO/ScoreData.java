d93d1ec949a0dd88627de7bb3d6fc9ee833b1847
hyeongwoo.lim <lkslyj2@naver.com>
2021-11-27 10:08:23 +0900

    delete: GameData.java functions are integrated into Game.java

--- src/baseball/DTO/ScoreData.java
+++ src/baseball/DTO/ScoreData.java
@@ -4,8 +4,18 @@ public class ScoreData {
     private int strike;
     private int ball;
 
+    public ScoreData() {}
+
     public ScoreData (int strike, int ball) {
         this.ball = ball;
         this.strike = strike;
+    }
+
+    public int getStrike() {
+        return this.strike;
+    }
+
+    public int getBall() {
+        return this.ball;
     }
 }
