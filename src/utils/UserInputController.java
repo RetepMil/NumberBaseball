4f881b8773c5f6c782748cf51cc8a96318f6d997
hyeongwoo.lim <lkslyj2@naver.com>
2021-11-27 12:26:23 +0900

    refractor: check user integer input digits

--- src/utils/UserInputController.java
+++ src/utils/UserInputController.java
@@ -3,6 +3,7 @@ package utils;
 import baseball.DTO.ScoreData;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
@@ -21,50 +22,44 @@ public class UserInputController {
         }
     }
 
-    public boolean askUserRestartGame(int yes_int, int no_int) {
+    public boolean askUserRestartGame(int yesInt, int noInt) {
         boolean boolVariable = false;
-        int userInput = Integer.parseInt(askForIntegerInput());
+        String userInput = askForIntegerInput();
 
-        if (userInput == yes_int) {
+        if (!isValidDigits(userInput, 1)) {
+            throw new IllegalArgumentException();
+        }
+
+        int userIntegerInput = Integer.parseInt(userInput);
+
+        if (userIntegerInput != yesInt && userIntegerInput != noInt) {
+            throw new IllegalArgumentException();
+        }
+
+        if (userIntegerInput == yesInt) {
             boolVariable = true;
         }
 
-        if (userInput == no_int) {
+        if (userIntegerInput == noInt) {
             boolVariable = false;
-        }
-
-        if (userInput != yes_int && userInput != no_int) {
-            throw new IllegalArgumentException();
         }
 
         return boolVariable;
     }
 
-    public int askUniqueInteger(int inclusive_lb, int inclusive_ub, ArrayList<Integer> history) {
-        int userInput = Integer.parseInt(askForIntegerInput());
-
-        if (userInput > inclusive_ub) {
-            throw new IllegalArgumentException();
-        }
-
-        if (userInput < inclusive_lb) {
-            throw new IllegalArgumentException();
-        }
-
-        if (history.stream().filter(a -> a == userInput).count() != 0) {
-            throw new IllegalArgumentException();
-        }
-
-        return userInput;
-    }
-
     public ArrayList<Integer> askUserGuessInput(int size) {
-        ArrayList<Integer> guessHistory = new ArrayList<Integer>(); //for duplicate checking
         System.out.print("숫자를 입력해주세요 (1~9 범위의 숫자"+ size + "개): ");
+        String userInput = askForIntegerInput();
+        if (!isValidDigits(userInput, size)) {
+            throw new IllegalArgumentException();
+        }
+        ArrayList<Integer> guesses = userInput.chars()
+                .mapToObj(Character::getNumericValue)
+                .collect(Collectors.toCollection(ArrayList::new));
         for (int i = 0; i < size; i++) {
             try {
-                int guess = askUniqueInteger(1, 9, guessHistory);
-                guessHistory.add(guess);
+                int guess = askUniqueInteger(1, 9, guesses);
+                guesses.add(guess);
             } catch (IllegalArgumentException e) {
                 System.out.print("비정상적인 입력");
                 System.exit(0);
@@ -73,14 +68,18 @@ public class UserInputController {
                 System.exit(0);
             }
         }
-        return guessHistory;
+        return guesses;
     }
 
     private boolean isInteger(String input) {
         return input.chars().allMatch(Character::isDigit);
     }
 
-    private checkForDuplicatedNumber(int numberInput) {
+    private boolean isValidDigits(String stringIntegerInput, int digitSize) {
+        return digitSize == stringIntegerInput.length();
+    }
+
+    private int isValidRange(int inclusive_lb, int inclusive_ub) {
 
     }
 }
